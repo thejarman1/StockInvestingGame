551d346ed94beec903b9025fcf5afc95882cc5b4
rrflowers45 <robertflowers@mail.weber.edu>
2022-07-07 00:49:53 -0600

    Displays max daily price based on ticker symbol entered

--- StockInvestingGame/Pages/Index.cshtml.cs
+++ StockInvestingGame/Pages/Index.cshtml.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using YahooFinanceApi;
+using System;
+using System.Linq;
+using ServiceStack;
+using ServiceStack.Text;
 
 namespace StockInvestingGame.Pages
 {
@@ -13,15 +16,39 @@ namespace StockInvestingGame.Pages
             _logger = logger;
         }
 
-
+        //Retrieves stock data in a clump and returns the data
+        //TODO: Add dateTime parameters, conditionals, and possibly chart creation (most likely a separate function)
         public IActionResult OnPostGetStocks(string value)
         {
-            return new JsonResult("Getting data for " + value);
+            var symbol = value; //Setting the symbol to what the user has entered
+            var apiKey = "YQ12ME2NUXQ29XG8"; //I got this key by registering my email. You all might wanna do the same or use mine?
+            var dailyPrices = $"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={apiKey}&datatype=csv"
+                .GetStringFromUrl().FromCsv<List<StockData>>();
+
+            dailyPrices.PrintDump();
+
+            var dayPrice = dailyPrices.Max(u => u.Close); //Getting max daily price. FOR TESTING
+            return new JsonResult("Getting data for " + value + "<br> Max Daily Price: $" + dayPrice);
         }
 
         public void OnGet()
         {
 
+        }
+
+        //********************CLASSES********************
+
+        //Stores stock data
+        public class StockData
+        {
+            public DateTime Timestamp { get; set; }
+            public decimal Open { get; set; }
+
+            public decimal High { get; set; }
+            public decimal Low { get; set; }
+
+            public decimal Close { get; set; }
+            public decimal Volume { get; set; }
         }
     }
 }
