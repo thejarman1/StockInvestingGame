564cb08224ac4523aae7455e9dfc427c3b7766fa
rrflowers45 <robertflowers@mail.weber.edu>
2022-07-07 12:46:40 -0600

    Can now specify date within the function for the ticker

--- StockInvestingGame/Pages/Index.cshtml.cs
+++ StockInvestingGame/Pages/Index.cshtml.cs
@@ -17,18 +17,21 @@ namespace StockInvestingGame.Pages
         }
 
         //Retrieves stock data in a clump and returns the data
-        //TODO: Add dateTime parameters, conditionals, and possibly chart creation (most likely a separate function)
+        //TODO: conditionals, possibly chart creation (most likely a separate function), randomizing the date
         public IActionResult OnPostGetStocks(string value)
         {
-            var symbol = value; //Setting the symbol to what the user has entered
+            var symbol = value; //Setting the ticker symbol to what the user has entered
             var apiKey = "YQ12ME2NUXQ29XG8"; //I got this key by registering my email. You all might wanna do the same or use mine?
             var dailyPrices = $"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={apiKey}&datatype=csv"
                 .GetStringFromUrl().FromCsv<List<StockData>>();
 
+            //This fills the object with values
             dailyPrices.PrintDump();
 
-            var dayPrice = dailyPrices.Max(u => u.Close); //Getting max daily price. FOR TESTING
-            return new JsonResult("Getting data for " + value + "<br> Max Daily Price: $" + dayPrice);
+            var testDate = new DateTime(2022, 7, 6); //This is a random date I chose for testing
+            var day = dailyPrices.Where(u => u.Timestamp.Year == testDate.Year && u.Timestamp.Month == testDate.Month && u.Timestamp.Day == testDate.Day); //This grabs the objects day
+            var dayPrice = day.Max(u => u.Close);//This gets the price
+            return new JsonResult("Getting data for " + value + "<br> Date: " + testDate + "<br> Closing price: $" + dayPrice);
         }
 
         public void OnGet()
