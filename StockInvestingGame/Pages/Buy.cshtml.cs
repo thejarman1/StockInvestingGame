46cc0f961a95639d88a9290c260c3d36a13fcfb0
kayleedockter <kayleedockter@mail.weber.edu>
2022-07-07 22:26:50 -0600

    Initial

---
+++ StockInvestingGame/Pages/Buy.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StockInvestingGame.Pages.Shared
+{
+    public class BuyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ StockInvestingGame/Pages/Sell.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StockInvestingGame.Pages
+{
+    public class SellModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
